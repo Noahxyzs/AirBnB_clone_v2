57279c6b0da658cae9456511aa0340172ba488fd
Noahxyzs <noahsolomon528@yahoo.com>
2023-07-22 11:40:36 -0700

    flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -14,14 +14,27 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return('Hello HBNB!')
+def hello():
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def redirect():
-    return('HBNB')
+def hbnb():
+    return "HBNB"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+@app.route('/c/<text>', strict_slashes=False)
+def cfun(text):
+    txt = text.replace('_', " ")
+    return "C {}". format(txt)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    txt = text.replace('_', " ")
+    return "Python {}". format(txt)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
